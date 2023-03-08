8ccb751d6910c2cc06acabbdccc253cbae36ec8b
Antonov333 <antonovsergei333@yahoo.com>
2023-03-08 17:15:30 +0300

    Just started with homework 9

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("\nHi!\n\nJust started with homework 9 dedicated to Massives topic\n");
     }
 }
